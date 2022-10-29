11907c2c2a9d77279d983cecf1b3cfe3d170284b
sumon <sumon137358@gmail.com>
2022-10-30 00:23:48 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,23 @@
+// falsify...... 
+
+// 0;
+// "";
+// undefine
+// null;
+// NaN;
+//false;
+// -0; 
+
+
+// truthy .......
+//"0"
+// " ";
+//[];
+//"false";
+let age =-0;
+if(age){
+    console.log("the condition is true");
+}
+else{
+    console.log("the condition is false");
+}
