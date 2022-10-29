0d3588a8153b8b61ad79ab879b36b0fb43690113
sumon <sumon137358@gmail.com>
2022-10-30 00:53:55 +0600

    some data added

---
+++ null-vs-undefine.js
@@ -0,0 +1,23 @@
+
+// undefined ......
+1//
+// function add (num1,num2){
+//     let result =num1 + num2 ;
+
+// }
+// const sum =add(2,3);
+// console.log(sum);// you can see undefined as you cannot return the resutl...
+
+2//
+// function add (num1,num2){
+//    console.log(num1 +num2);
+// }
+// const sum =add(2);//undefined as you can get this one value..
+// console.log(sum);
+
+3//
+// const num ={name:"sumon",number:222};
+// console.log(num.roll);//as you con not set the roll property..
+
+const num =[2,4,5];
+console.log(num[33]);
